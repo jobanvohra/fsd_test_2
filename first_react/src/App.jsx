495706f4d95ce1dc6199fd68f468754e7816b997
jobanvohra <vohrajoban5@gmail.com>
2025-11-06 15:01:20 +0530

    add Card component

--- first_react/src/App.jsx
+++ first_react/src/App.jsx
@@ -1,10 +1,25 @@
-import Vehicle from './components/Vehicle';
+import Card from './components/Card';
 
 function App() {
 	return (
 		<>
-			<Vehicle />
-			<h1>Testing component</h1>
+			<div className="card-container">
+				<Card
+					image="/image1.jpg"
+					title="React Basics"
+					description="Learn about components,props and state."
+				/>
+				<Card
+					image="https://picsum.photos/200/151"
+					title="JavaScript"
+					description="Master the language behind React."
+				/>
+				<Card
+					image="https://picsum.photos/200/152"
+					title="CSS Tricks"
+					description="Style your components beautifully."
+				/>
+			</div>
 		</>
 	);
 }
---
+++ first_react/src/components/Card.jsx
@@ -0,0 +1,16 @@
+function Card(props) {
+	return (
+		<div className="card">
+			<img
+				width={'200px'}
+				src={props.image}
+				alt={props.title}
+				className="card-image"
+			/>
+			<h3 className="card-title">{props.title}</h3>
+			<p className="card-description">{props.description}</p>
+		</div>
+	);
+}
+
+export default Card;
